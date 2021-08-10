90b9711234c966e6bda340fcf4c44bdad8d448c3
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-10 11:05:11 -0700

    added Links to each vending machine item

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -3,18 +3,25 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const VendingMachine = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Route exact path="/chocolatebar">
-          <ChocolateBar />
-        </Route>
-        <Route exact path="/skittles">
-          <Skittles />
-        </Route>
-        <Route exact path="/snickers">
-          <Snickers />
-        </Route>
-      </BrowserRouter>
+    <div className="VendingMachine">
+      <nav>
+        <Link to="/chocolatebar">Chocolate Bar</Link>
+        <Link to="/skittles">Skittles</Link>
+        <Link to="/snickers">Snickers</Link>
+      </nav>
+      <main>
+        <BrowserRouter>
+          <Route exact path="/chocolatebar">
+            <ChocolateBar />
+          </Route>
+          <Route exact path="/skittles">
+            <Skittles />
+          </Route>
+          <Route exact path="/snickers">
+            <Snickers />
+          </Route>
+        </BrowserRouter>
+      </main>
     </div>
   )
 };
