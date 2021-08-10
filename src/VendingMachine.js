1cacfc1708764c0e2b2f02afb51870bbda6e3f80
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-10 11:01:36 -0700

    added in routes for vending machine items

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Router, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const VendingMachine = () => {
-
+  return (
+    <div>
+      <BrowserRouter>
+        <Route exact path="/chocolatebar">
+          <ChocolateBar />
+        </Route>
+        <Route exact path="/skittles">
+          <Skittles />
+        </Route>
+        <Route exact path="/snickers">
+          <Snickers />
+        </Route>
+      </BrowserRouter>
+    </div>
+  )
 };
 
 export default VendingMachine;
