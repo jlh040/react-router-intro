e641c425ff62091f1ad2b25c6e570dd7cb37041e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-10 11:11:09 -0700

    created a chocolate bar component and fixed the vending machine component

---
+++ src/ChocolateBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChocolateBar = () => {
+
+};
+
+export default ChocolateBar;
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ChocolateBar from './ChocolateBar';
 
 const VendingMachine = () => {
   return (
     <div className="VendingMachine">
-      <nav>
-        <Link to="/chocolatebar">Chocolate Bar</Link>
-        <Link to="/skittles">Skittles</Link>
-        <Link to="/snickers">Snickers</Link>
-      </nav>
-      <main>
-        <BrowserRouter>
+      <BrowserRouter>
+        <nav>
+          <Link to="/chocolatebar">Chocolate Bar</Link>
+          <Link to="/skittles">Skittles</Link>
+          <Link to="/snickers">Snickers</Link>
+        </nav>
+        <main>
           <Route exact path="/chocolatebar">
             <ChocolateBar />
           </Route>
-          <Route exact path="/skittles">
+          {/* <Route exact path="/skittles">
             <Skittles />
           </Route>
           <Route exact path="/snickers">
             <Snickers />
-          </Route>
-        </BrowserRouter>
-      </main>
+          </Route> */}
+        </main>
+      </BrowserRouter>
     </div>
   )
 };
