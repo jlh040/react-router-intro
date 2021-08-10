17e85d535fbfa763d510c731524a0a91f74928dc
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-10 11:55:00 -0700

    fixed an error with the fonts

--- src/Donut.js
+++ src/Donut.js
@@ -5,7 +5,7 @@ import donut from './donut.jpeg';
 const Donut = () => {
   return (
     <div>
-      <h2>Time to eat a donut!!!</h2>
+      <h1>Time to eat a donut!!!</h1>
       <img src={donut} alt="donut on table" />
       <Link to="/">Go Back</Link>
     </div>
