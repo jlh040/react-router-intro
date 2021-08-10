5a1b16469c3540d782c9c1432992aff9ad5f9b0f
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-10 12:54:11 -0700

    added a stylesheet to change the 'go back' link on the item pages

--- src/Donut.js
+++ src/Donut.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import donut from './donut.jpeg';
+import './Donut.css';
 
 const Donut = () => {
   return (
--- src/SnackItems.js
+++ src/SnackItems.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './SnackItems.css';
 
 
 const SnackItems = () => {
   return (
     <nav className="SnackItems">
+      <h1>Vending Machine</h1>
       <Link exact to="/chocolatebar">Chocolate Bar</Link>
       <Link exact to="/skittles">Skittles</Link>
       <Link exact to="/donut">Donut</Link>
